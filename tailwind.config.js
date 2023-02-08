b543f166bcf9f787303b5f05405118930be3d62c
Thiện Mù <104013686+ThienSimp03@users.noreply.github.com>
2023-02-08 13:52:34 +0700

    config-pr-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 module.exports = {
   content: ["./scripts/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fromLeftToRight: "fromLeftToRight 1s linear 1 forwards",
+        fromRightToLeft: "fromRightToLeft 1s linear 1 forwards",
+      },
+      keyframes: {
+        fromLeftToRight: {
+          from: {
+            filter: "blur(33px)",
+            opacity: "0",
+            transform: "translate(-50%, 0)",
+          },
+          to: {
+            filter: " blur(0)",
+            opacity: "1",
+            transform: "translate(0, 0)",
+          },
+        },
+        fromRightToLeft: {
+          from: {
+            filter: "blur(33px)",
+            opacity: "0",
+            transform: "translate(0, 50%)",
+          },
+          to: {
+            filter: " blur(0)",
+            opacity: "1",
+            transform: "translate(0, 0)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
