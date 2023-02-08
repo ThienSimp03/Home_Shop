d53680ff1fba8cd948799989b658551a0b7d1721
Thiện Mù <104013686+ThienSimp03@users.noreply.github.com>
2023-02-08 20:38:32 +0700

    add animate.css

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,12 @@ let parallax = document.querySelectorAll(".parallaxScroll");
 let listProductHot = document.querySelectorAll(
   "#itemHotProduct div[data-productHot]"
 );
-listProductHot[0].style.display = "block";
 let productHot = document.querySelector("#productHot");
 let checkProductHotActive = false;
 function autoScrollSlide(list) {
   let lengthList = list.length,
     currIndex = 0;
-  // list[currIndex].style.display = "block";
+  list[currIndex].style.display = "block";
   setInterval(() => {
     list[currIndex].style.display = "none";
     ++currIndex;
