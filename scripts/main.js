98addb92ea3a37ee205990d6471867862a5931d2
Thiện Mù <104013686+ThienSimp03@users.noreply.github.com>
2023-02-09 11:49:56 +0700

    change-5

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ let listProductHot = document.querySelectorAll(
   "#itemHotProduct div[data-productHot]"
 );
 let productHot = document.querySelector("#productHot");
+let navgation = document.querySelector("#nav");
 let checkProductHotActive = false;
 function autoScrollSlide(list) {
   let lengthList = list.length,
@@ -15,10 +16,27 @@ function autoScrollSlide(list) {
       currIndex = 0;
     }
     list[currIndex].style.display = "block";
-  }, 3000);
+  }, 5000);
 }
 // Parallax scroll
 document.addEventListener("scroll", () => {
+  // Nav Active
+  activeMenu();
+  // End Nav Active
+  // Nav
+  if (window.scrollY > 200) {
+    navgation.classList.add(
+      "toFixed",
+      "animate__slideInDown",
+      "animate__animated"
+    );
+  } else
+    navgation.classList.remove(
+      "toFixed",
+      "animate__slideInDown",
+      "animate__animated"
+    );
+  // End Nav
   if (productHot.offsetTop - window.scrollY < 350 && !checkProductHotActive) {
     autoScrollSlide(listProductHot);
     checkProductHotActive = true;
@@ -26,7 +44,20 @@ document.addEventListener("scroll", () => {
   parallax.forEach((item) => {
     if (item.offsetTop - window.scrollY < 350) {
       item.classList.add("active");
-    }
+    } else item.classList.remove("active");
   });
 });
 // End Parallax scroll
+// Active Menu
+let navgationActive = document.querySelectorAll("#nav .links");
+let selector = document.querySelectorAll(".selector");
+function activeMenu() {
+  let length = selector.length;
+  while (--length && selector[length].offsetTop > window.scrollY + 100) {}
+  navgationActive.forEach((item) => {
+    item.classList.remove("activeNav");
+  });
+  navgationActive[length].classList.add("activeNav");
+}
+activeMenu();
+// End Active Menu
